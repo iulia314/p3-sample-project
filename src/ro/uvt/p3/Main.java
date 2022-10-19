b855a51534d203d09671df03f73fc0bb7f13ffd1
iuliapopa4 <Iuliapopa172@gmail.com>
2022-10-19 16:52:57 +0300

    ...

--- src/ro/uvt/p3/Main.java
+++ src/ro/uvt/p3/Main.java
@@ -4,12 +4,11 @@ import ro.uvt.p3.io.InputDevice;
 import ro.uvt.p3.io.OutputDevice;
 
 public class Main {
-
     public static void main(String[] args) {
         InputDevice id = new InputDevice();
         OutputDevice od = new OutputDevice();
         Application app = new Application(id, od);
-
+        System.out.println("Hello world!");
         app.run();
     }
 }
